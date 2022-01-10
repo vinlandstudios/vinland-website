201416c1a7d67d3519d7b63c9b15542b0efb0028
Tahir Ahmad <tahirahmad.in@gmail.com>
2022-01-10 10:06:26 +0530

    changes in npm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-import Appbar from "./common/Appbar";
 import Home from "./pages/Home/Home";
 import "./App.css";
 import { ThemeProvider } from "@material-ui/core/styles";
